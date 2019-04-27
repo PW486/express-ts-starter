fec208499ad2904df107520ece92279557568873
DONGGEON LIM <ooqwe486@gmail.com>
2019-04-27 14:03:53 +0000

    Update typeorm configuration

--- src/config/environment.ts
+++ src/config/environment.ts
@@ -1,9 +1,58 @@
 import { config } from 'dotenv';
 import { existsSync } from 'fs';
+import { ConnectionOptions } from 'typeorm';
 
 existsSync('.env') ? config() : config({ path: '.env.example' });
 
 export const NODE_ENV = process.env.NODE_ENV || 'development';
 export const PORT = Number(process.env.PORT) || 3000;
-export const DB_HOST = process.env.DB_HOST;
-export const DB_PASSWORD = process.env.DB_PASSWORD;
+
+export const DB_CONFIG: { [name: string]: ConnectionOptions } = {
+  "development": {
+    "type": "postgres",
+    "host": "localhost",
+    "port": 5432,
+    "username": "dev",
+    "password": "dev",
+    "database": "dev",
+    "synchronize": true,
+    "entities": [
+      "dist/api/**/*.entity.js"
+    ],
+    "logging": [
+      "error",
+      "query"
+    ]
+  },
+  "test": {
+    "type": "postgres",
+    "host": "localhost",
+    "port": 5432,
+    "username": "test",
+    "password": "test",
+    "database": "test",
+    "synchronize": true,
+    "entities": [
+      "dist/api/**/*.entity.js"
+    ],
+    "logging": [
+      "error"
+    ]
+  },
+  "production": {
+    "name": "production",
+    "type": "postgres",
+    "host": process.env.DB_HOST,
+    "port": 5432,
+    "username": "postgres",
+    "password": process.env.DB_PASSWORD,
+    "database": "prod",
+    "synchronize": false,
+    "entities": [
+      "dist/api/**/*.entity.js"
+    ],
+    "logging": [
+      "error"
+    ]
+  }
+};
--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,18 @@
 import "reflect-metadata";
 import { createConnection } from "typeorm";
 import express from 'express';
-import { NODE_ENV, PORT } from './config/environment';
+import { NODE_ENV, PORT, DB_CONFIG } from './config/environment';
 import { initRoute } from './config/route';
 import { initMiddleware } from './config/middleware';
 
-createConnection(NODE_ENV).then(async connection => {
-}).catch(error => console.log("TypeORM connection error: ", error));
+const server = async () => {
+  await createConnection(DB_CONFIG[NODE_ENV]);
 
-const app = express();
-initMiddleware(app);
-initRoute(app);
+  const app = express();
+  await initMiddleware(app);
+  await initRoute(app);
 
-app.listen(PORT, () => {
-  console.log('app listening on port 3000');
-});
+  app.listen(PORT);
+}
+
+server();
