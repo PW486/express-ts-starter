2e5ca31378d01bbe3869421c307f38f3b317ffdc
DONGGEON LIM <ooqwe486@gmail.com>
2019-05-09 03:59:12 +0000

    Create errorHandler

--- src/app.ts
+++ src/app.ts
@@ -1,25 +1,12 @@
 import express from 'express';
-import { NextFunction, Request, Response } from 'express';
+import { mountErrorHandler } from './config/errorHandler';
 import { mountMiddlewares } from './config/middlewares';
 import { mountRoutes } from './config/routes';
-import { isCelebrate } from 'celebrate';
 
 const app = express();
 
 mountMiddlewares(app);
 mountRoutes(app);
-
-app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
-  if (err.name === 'UnauthorizedError') {
-    err.statusCode = 401;
-  } else if (err.name === 'MulterError') {
-    err.statusCode = 400;
-  } else if (isCelebrate(err)) {
-    err.statusCode = 400;
-    err.message = err.joi.message;
-  }
-
-  res.status(err.statusCode || 500).json({ message: err.message });
-});
+mountErrorHandler(app);
 
 export default app;
