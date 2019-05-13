f7983bcdbd52802d63f2427d4a300ff15fbaf987
DONGGEON LIM <ooqwe486@gmail.com>
2019-05-13 11:29:37 +0000

    Move reflect-metadata

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import 'reflect-metadata';
 import { mountErrorHandlers } from './config/errorHandlers';
 import { mountMiddlewares } from './config/middlewares';
 import { mountRoutes } from './config/routes';
--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,3 @@
-import 'reflect-metadata';
 import { createConnection } from 'typeorm';
 import app from './app';
 import { DB_CONFIG, NODE_ENV, PORT } from './config/environments';
