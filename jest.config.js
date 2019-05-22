c279e9f2d1106ff7e527058160eaae1494c44fd1
DONGGEON LIM <ooqwe486@gmail.com>
2019-05-22 02:58:56 +0000

    Remove unused imports

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,3 @@
-const { pathsToModuleNameMapper } = require('ts-jest/utils');
-const { compilerOptions } = require('./tsconfig.json');
-
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
