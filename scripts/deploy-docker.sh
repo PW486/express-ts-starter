3121ef6e9f29e15d4ddc4cfffd53fe228f267637
DONGGEON LIM <ooqwe486@gmail.com>
2019-06-16 01:53:39 +0000

    Test travis

--- scripts/deploy-docker.sh
+++ scripts/deploy-docker.sh
@@ -1,4 +1,12 @@
 docker swarm init
-echo "secret" | docker secret create db_password -
-echo "secret" | docker secret create jwt_secret -
+docker service create --name registry --publish published=5000,target=5000 registry:2
+docker service ls
+
+echo "dbsecret" | docker secret create db_password -
+echo "jwtsecret" | docker secret create jwt_secret -
+
+docker-compose -f docker-compose-prod.yml build
+docker-compose -f docker-compose-prod.yml push
+
 docker stack deploy -c docker-compose-prod.yml express_ts
+docker stack services express_ts
