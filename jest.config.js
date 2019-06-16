eec2af318a2b6e97a512d64e45af9ae7fe43b99a
DONGGEON LIM <ooqwe486@gmail.com>
2019-06-16 01:24:10 +0000

    Change jest config, scripts

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,9 @@
 module.exports = {
+  collectCoverage: true,
   moduleDirectories: ['node_modules', 'dist'],
   preset: 'ts-jest',
   setupFilesAfterEnv: ['./jest.setup.js'],
   testEnvironment: 'node',
   testMatch: ['**/dist/**/?(*.)+(spec|test).js?(x)'],
+  verbose: true,
 };
---
+++ scripts/clean-package.sh
@@ -0,0 +1,5 @@
+rm -rf ./node_modules
+rm -rf ./coverage
+rm -rf ./dist
+rm -rf ./logs
+rm -rf ./uploads
